fdf39bb526a7d65c8f279dd8b38a180fb80255f5
adwu28@gmail.com <adwu28@gmail.com>
2024-05-31 21:48:09 -0400

    App should now render game component only when game has been created or joined

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,79 @@
-import { useState } from "react";
-import { Container, TextField } from "@mui/material";
+import { useEffect, useState, useCallback } from "react";
+import Container from "@mui/material/Container";
 import Game from "./Game";
-import socket from './socket';
+import InitGame from "./InitGame";
 import CustomDialog from "./components/CustomDialog";
+import socket from "./socket";
+import { TextField } from "@mui/material";
 
 export default function App() {
-  const [username, setUsername] = useState('');
-
-  // indicates if a username has been submitted
+  const [username, setUsername] = useState("");
   const [usernameSubmitted, setUsernameSubmitted] = useState(false);
+
+  const [room, setRoom] = useState(""); // stores current room ID
+  const [orientation, setOrientation] = useState(""); // stores board orientiation for user
+  const [players, setPlayers] = useState([]); //stores players in room
+
+  // resets the states responsible for initializing a game
+  const cleanup = useCallback(() => {
+    setRoom("");
+    setOrientation("");
+    setPlayers("");
+  }, []);
+
+  useEffect(() => {
+    // const username = prompt("Username");
+    // setUsername(username);
+    // socket.emit("username", username);
+
+    socket.on("opponentJoined", (roomData) => {
+      console.log("roomData", roomData)
+      setPlayers(roomData.players);
+    });
+  }, []);
 
   return (
     <Container>
       <CustomDialog
-        open={!usernameSubmitted} // leave open if username has not been selected
-        title="Pick a username" // Title of dialog
-        contentText="Please select a username" // content text of dialog
-        handleContinue={() => { // fired when continue is clicked
-          if (!username) return; // if username hasn't been entered, do nothing
-          socket.emit("username", username); // emit a websocket event called "username" with the username as data
-          setUsernameSubmitted(true); // indicate that username has been submitted
+        open={!usernameSubmitted}
+        handleClose={() => setUsernameSubmitted(true)}
+        title="Pick a username"
+        contentText="Please select a username"
+        handleContinue={() => {
+          if (!username) return;
+          socket.emit("username", username);
+          setUsernameSubmitted(true);
         }}
       >
-        <TextField // Input
-          autoFocus // automatically set focus on input (make it active).
+        <TextField
+          autoFocus
           margin="dense"
           id="username"
           label="Username"
           name="username"
           value={username}
           required
-          onChange={(e) => setUsername(e.target.value)} // update username state with value
+          onChange={(e) => setUsername(e.target.value)}
           type="text"
           fullWidth
           variant="standard"
         />
       </CustomDialog>
-      <Game />
+      {room ? (
+        <Game
+          room={room}
+          orientation={orientation}
+          username={username}
+          players={players}
+          cleanup={cleanup} // cleans up state when game is over
+        />
+      ) : (
+        <InitGame
+          setRoom={setRoom}
+          setOrientation={setOrientation}
+          setPlayers={setPlayers}
+        />
+      )}
     </Container>
   );
 }
